6673f4086ddcc627905d58c797cc1a2095ab5bc2
Fahad Javed <fahad.js@yahoo.com>
2024-04-16 19:28:35 +0500

    updated cpanel db

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mah_portal');
+define('DB_HOST', '162.240.101.166');
+define('DB_USER', 'a0786website_mah_portal');
+define('DB_PASS', 'lqEnBGmbJ,YO');
+define('DB_NAME', 'a0786website_mah_portal');
