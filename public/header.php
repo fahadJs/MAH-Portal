188bdfba7f712f417c08a1b08b3653eaf758e9fd
fahadjs <fahad.js@yahoo.com>
2024-05-02 22:20:12 +0500

    added nav link

--- public/header.php
+++ public/header.php
@@ -63,6 +63,10 @@
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Manage Deliveries
                         </a>
+                        <a class="nav-link" href="daily-status.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Update Center
+                        </a>
                         <!-- <div class="sb-sidenav-menu-heading">Interface</div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
